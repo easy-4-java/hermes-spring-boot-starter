f432741cdd3693e357213cf1a486f6ea12309387
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-11 12:29:06 +0800

    feat: bind unified SDK debug levels

--- src/test/java/io/github/easy4j/hermes/spring/boot/HermesAutoConfigurationTest.java
+++ src/test/java/io/github/easy4j/hermes/spring/boot/HermesAutoConfigurationTest.java
@@ -20,7 +20,10 @@ class HermesAutoConfigurationTest {
             .withUserConfiguration(HermesAutoConfiguration.class)
             .withPropertyValues(
                     "hermes.http.startup-check-enabled=false",
-                    "hermes.cli.startup-check-enabled=false");
+                    "hermes.cli.startup-check-enabled=false",
+                    "hermes.debug.enabled=true",
+                    "hermes.debug.level=HEADERS",
+                    "hermes.debug.max-content-length=4096");
 
     @Test
     void shouldCreateHighConcurrencyDefaults() {
@@ -30,9 +33,15 @@ class HermesAutoConfigurationTest {
             assertEquals(10_000, client.writeTimeoutMillis());
             assertEquals(120_000, client.readTimeoutMillis());
             assertEquals(128, client.dispatcher().getMaxRequests());
-            assertEquals(64, client.dispatcher().getMaxRequestsPerHost());
+            assertEquals(128, client.dispatcher().getMaxRequestsPerHost());
             assertEquals(2_000, context.getBean(HermesHttpClientConfig.class).getConnectTimeoutMillis());
             assertEquals(300, context.getBean(HermesCliConfig.class).getTimeout());
+            HermesProperties properties = context.getBean(HermesProperties.class);
+            assertEquals(true, properties.getDebug().isEnabled());
+            assertEquals("HEADERS", properties.getDebug().getLevel().name());
+            assertEquals(4096, properties.getDebug().getMaxContentLength());
+            assertSame(properties.getDebug(), properties.getHttp().getDebug());
+            assertSame(properties.getDebug(), properties.getCli().getDebug());
             assertSame(client, context.getBean(HermesClient.class).getOkHttpClient());
         });
     }
